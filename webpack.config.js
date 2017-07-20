bd6406b46a9a4d6d8a5f173f16789fb1003fc7a5
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-20 13:58:38 +0300

    first commit - configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack';
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                loaders: ['babel-loader'],
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                test: /\.js$/
+            }
+        ]
+    }
+};
